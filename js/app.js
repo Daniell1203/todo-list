5e093154804e43291aee66167154a258becd2c09
Carlos Daniel <danieldpaula07@gmail.com>
2026-04-08 19:36:41 -0300

    feat: implementar persistência e remoção de tarefas com localStorage

--- js/app.js
+++ js/app.js
@@ -9,11 +9,41 @@ function adicionarTarefa() {
 
   li.addEventListener("click", function () {
     li.remove();
+
+    let tarefas = pegarTarefas();
+    tarefas = tarefas.filter(tarefa => tarefa !== li.textContent);
+    salvarTarefas(tarefas);
   });
 
   lista.appendChild(li);
 
+  let tarefas = pegarTarefas();
+  tarefas.push(input.value);
+  salvarTarefas(tarefas);
+
   input.value = "";
 }
+
+function carregarTarefas() {
+  const tarefas = pegarTarefas();
+  const lista = document.getElementById("listaTarefas");
+
+  tarefas.forEach(tarefa => {
+    const li = document.createElement("li");
+    li.textContent = tarefa;
+
+    li.addEventListener("click", function () {
+      li.remove();
+
+      let tarefasAtualizadas = pegarTarefas();
+      tarefasAtualizadas = tarefasAtualizadas.filter(t => t !== tarefa);
+      salvarTarefas(tarefasAtualizadas);
+    });
+
+    lista.appendChild(li);
+  });
+}
+
+carregarTarefas();
 
 window.adicionarTarefa = adicionarTarefa;
